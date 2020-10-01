9f40b5aaccb83ba6c9984660053be5f38a738313
Yura Pogorelov <pyurik44@gmail.com>
2020-10-01 13:16:10 +0300

    удалил талицу color

--- car_blog/admin.py
+++ car_blog/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from car_blog.models import Color, Transmission, Body_type, Brend
+from car_blog.models import Transmission, Body_type, Brend
 
-admin.site.register(Color)
 admin.site.register(Transmission)
 admin.site.register(Body_type)
 admin.site.register(Brend)
--- car_blog/models.py
+++ car_blog/models.py
@@ -2,17 +2,6 @@ from django.db import models
 
 
 # Create your models here.
-class Color(models.Model):
-    name = models.CharField("Color", max_length=150)
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        verbose_name = "Цвет"
-        verbose_name_plural = "Цвета"
-
-
 class Transmission(models.Model):
     name = models.CharField("Трансмиссия", max_length=200)
 
