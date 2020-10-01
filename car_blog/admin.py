6b21906ff9df49aba9e0389717c407740cf5bb42
Yura Pogorelov <pyurik44@gmail.com>
2020-10-01 13:12:48 +0300

    добавлены таблицы body_type and brend

--- car_blog/admin.py
+++ car_blog/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from car_blog.models import Color
+from car_blog.models import Color, Transmission, Body_type, Brend
 
 admin.site.register(Color)
+admin.site.register(Transmission)
+admin.site.register(Body_type)
+admin.site.register(Brend)
--- car_blog/models.py
+++ car_blog/models.py
@@ -22,3 +22,28 @@ class Transmission(models.Model):
     class Meta:
         verbose_name = "Трасмиссия"
         verbose_name_plural = "Трансмисии"
+
+
+class Body_type(models.Model):
+    name = models.CharField("Типы кузова", max_length=200)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Тип кузова"
+        verbose_name_plural = "Типы кузовов"
+
+
+class Brend(models.Model):
+    name = models.CharField("Название компании", max_length=200)
+    tel = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Название компании"
+        verbose_name_plural = "Название компаний"
+
+# class Model(models.Model):
