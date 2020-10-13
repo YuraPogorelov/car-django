07afd7e3daf69111e7255c2ae60c35ccc3b767fe
YuraPogorelov <pogorelov.yurik@gmail.com>
2020-10-13 22:53:29 +0300

    Добавил Model и произвел коректировку таблиц

--- car_blog/models.py
+++ car_blog/models.py
@@ -49,8 +49,20 @@ class Engine(models.Model):
         verbose_name_plural = "Типы длигателей"
 
 
+class Model(models.Model):
+    brend = models.ForeignKey(Brend, verbose_name="Фирма", on_delete=models.CASCADE, null=True)
+    name = models.CharField("Марка", max_length=255)
+
+    def __str__(self):
+        return f"{self.brend} - {self.name}"
+
+    class Meta:
+        verbose_name = "Модель"
+        verbose_name_plural = "Модели"
+
+
 class Characteristics(models.Model):
-    model = models.ForeignKey(Brend, verbose_name='Модели', on_delete=models.SET_NULL, null=True, blank=True)
+    model = models.ForeignKey(Model, verbose_name='Модели', on_delete=models.CASCADE, null=True, blank=True)
     image = models.ImageField("Фото", upload_to="image/", null=True)
     color = models.CharField("Цвет авто", max_length=200, null=True)
     transmission = models.ForeignKey(Transmission, verbose_name="Тип коробки передач", on_delete=models.CASCADE, null=True)
@@ -62,8 +74,8 @@ class Characteristics(models.Model):
     price = models.CharField("Стоимость", max_length=100, null=True)
 
     def __str__(self):
-        # return self.model()
-        return f"{self.model} - {self.transmission}"
+        # return self.model
+        return f"{self.model} - {self.price}"
 
     class Meta:
         verbose_name = "Основные характеристики машины"
