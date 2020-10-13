4b4b807e82debe72ad11fcd84ce67df1c8443f0a
YuraPogorelov <pogorelov.yurik@gmail.com>
2020-10-13 22:53:39 +0300

    Добавил Model и произвел коректировку таблиц

--- car_blog/admin.py
+++ car_blog/admin.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 
 # Register your models here.
 
-from car_blog.models import Transmission, Body_type, Brend, Engine, Characteristics
+from car_blog.models import Transmission, Body_type, Brend, Engine, Characteristics, Model
 
 admin.site.register(Transmission)
 admin.site.register(Body_type)
 admin.site.register(Brend)
 admin.site.register(Engine)
 admin.site.register(Characteristics)
+admin.site.register(Model)
