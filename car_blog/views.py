e81c75226f56d3466dd4b868d94a79f0362aaabe
YuraPogorelov <pogorelov.yurik@gmail.com>
2020-10-16 00:41:27 +0300

    Сделал view Для оборажение списка авто. Настроил url.

--- car_blog/views.py
+++ car_blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic.base import View
+
+from .models import Characteristics
+
+
+class CharacteristicsView(View):
+
+    def get(self, request):
+        characteristics = Characteristics.objects.all()
+        return render(request, "characteristics/characteristics_list.html", {"characteristics_list": characteristics})
